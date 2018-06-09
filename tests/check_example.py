0c8e52820e6a62de837ed6e46378d8f7068a9363
Jack Grahl <jack.grahl@gmail.com>
2018-06-09 22:16:10 +0000

    Check examples for factory and custom examples.

--- tests/check_example.py
+++ tests/check_example.py
@@ -12,7 +12,7 @@ class TestApp(object):
         self.client = self.app.test_client()
 
     def test_output(self):
-        r = self.client.get("/doc")
+        r = self.client.get(self.path)
         self.assertEqual(r.status_code, 200)
         data = r.data.decode('utf-8')
         with open(self.filename) as f:
@@ -24,10 +24,12 @@ class TestApp(object):
 class TestSimpleApp(TestApp, unittest.TestCase):
     app = simple_app
     filename = "tests/files/simple.html"
+    path = "/doc"
 
 
 class TestFactoryApp(TestApp, unittest.TestCase):
     filename = "tests/files/factory.html"
+    path = "/doc/"
 
     @classmethod
     def setUpClass(cls):
@@ -39,6 +41,7 @@ class TestFactoryApp(TestApp, unittest.TestCase):
 class TestCustomApp(TestApp, unittest.TestCase):
     app = custom_app
     filename = "tests/files/custom.html"
+    path = "/doc/"
 
 
 if __name__ == "__main__":
