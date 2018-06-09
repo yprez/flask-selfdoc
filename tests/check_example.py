19eff22e59af9c5bef6d7f21dbaea2f2e08c2d09
Jack Grahl <jack.grahl@gmail.com>
2018-06-09 21:58:00 +0000

    Refactor this test class for generalization.

--- tests/check_example.py
+++ tests/check_example.py
@@ -3,21 +3,25 @@ import unittest
 from examples.simple.blog import app as simple_app
 
 
-class TestSimpleApp(unittest.TestCase):
+class TestApp(object):
     maxDiff = None
 
     def setUp(self):
-        self.app = simple_app
         self.client = self.app.test_client()
 
     def test_output(self):
         r = self.client.get("/doc")
         self.assertEqual(r.status_code, 200)
         data = r.data.decode('utf-8')
-        with open("tests/files/simple.html") as f:
+        with open(self.filename) as f:
             expected = f.read()
 
         self.assertEqual(data, expected)
+
+
+class TestSimpleApp(TestApp, unittest.TestCase):
+    app = simple_app
+    filename = "tests/files/simple.html"
 
 
 if __name__ == "__main__":
